908e1bb251e9e510735731a52e4074a29bb315eb
Bin Zhang <bin.zhang@iocs-systems.com>
2016-06-23 15:44:31 -0400

    f2 commit 02

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 class Test {
   public static void main(String[] arg) {
       // TODO
-	  System.out.println("Added by f2");
+	  System.out.println("Added by f2 0");
+	  System.out.println("Added by f2 1");
+	  System.out.println("Added by f2 2");
   }
  
 }
